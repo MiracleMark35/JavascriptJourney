566e54c0df27516b32b8b714128fa79138d5c953
Mark-Cloud-ph <markianfrost123@gmail.com>
2026-02-09 21:50:28 +0800

    logger

--- Express/CrashCourse/server.js
+++ Express/CrashCourse/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import posts from './router/post.js'
+import logger from './middleware/logger.js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -12,7 +13,7 @@ const PORT = process.env.PORT || 8000;
 // app is basically what we use for routes, middleware , listening
 const app  = express()
 
-
+app.use(logger)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use( '/api/posts' , posts)
